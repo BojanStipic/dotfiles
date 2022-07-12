88385d71cb7ee842e2467de4a5e708e29ed82158
BojanStipic <bojanstipic@gmail.com>
2022-07-12 21:54:15 +0200

    [nvim] add vim global to sumneko_lua

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -489,6 +489,9 @@ require('lspconfig').sumneko_lua.setup(vim.tbl_extend('force', lsp_opts, {
             runtime = {
                 version = 'LuaJIT',
             },
+            diagnostics = {
+                globals = { 'vim' },
+            },
             workspace = {
                 library = vim.api.nvim_get_runtime_file('', true),
             },
