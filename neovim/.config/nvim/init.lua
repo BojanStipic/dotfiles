70cfcc43089e96043932d1a541fc59712f01ee57
BojanStipic <bojanstipic@gmail.com>
2022-10-16 22:08:51 +0200

    [nvim] Use the latest cmp-nvim-lsp api

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -472,9 +472,7 @@ end
 
 local lsp_opts = {
     on_attach = lsp_on_attach,
-    capabilities = require('cmp_nvim_lsp').update_capabilities(
-        vim.lsp.protocol.make_client_capabilities()
-    ),
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
 }
 
 require('lspconfig').rust_analyzer.setup(lsp_opts)
