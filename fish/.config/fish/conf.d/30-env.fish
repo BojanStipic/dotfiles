d11462f1f1659f8f5773ca0c41d7a1670d1ac7e3
BojanStipic <bojanstipic@gmail.com>
2024-02-22 16:20:55 +0100

    [fish] Reorganize config files

--- fish/.config/fish/conf.d/30-env.fish
+++ fish/.config/fish/conf.d/30-env.fish
@@ -1,9 +1,5 @@
 status is-interactive || exit 0
 
-fish_add_path --path --move ~/.local/bin ~/.cargo/bin ~/.npm/bin ~/.go/bin
-set --global --export npm_config_prefix ~/.npm
-set --global --export GOPATH ~/.go
-
 set --global --export VISUAL nvim
 set --global --export DIFFPROG 'nvim -d'
 set --global --export MANPAGER 'nvim +Man!'
