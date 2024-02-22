1230306a69451d50bf9d88c9d8e57154c4f69b55
BojanStipic <bojanstipic@gmail.com>
2024-02-22 15:45:12 +0100

    [fish] Use fish_user_paths var instead of PATH directly

--- fish/.config/fish/conf.d/20-env.fish
+++ fish/.config/fish/conf.d/20-env.fish
@@ -1,6 +1,6 @@
 status is-interactive || exit 0
 
-fish_add_path --path ~/.local/bin ~/.cargo/bin ~/.npm/bin ~/.go/bin
+fish_add_path ~/.local/bin ~/.cargo/bin ~/.npm/bin ~/.go/bin
 set --global --export npm_config_prefix ~/.npm
 set --global --export GOPATH ~/.go
 
