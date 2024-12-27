d53f1075e765a983d7cb6d637e9cd413a4ddab40
BojanStipic <bojanstipic@gmail.com>
2024-12-27 13:41:43 +0100

    [fish] Read fish path from /etc/shells

--- fish/.config/fish/conf.d/10-system.fish
+++ fish/.config/fish/conf.d/10-system.fish
@@ -1,5 +1,5 @@
 status is-interactive || exit 0
 
 if status is-login && test -e /etc/profile
-    exec bash -c 'source /etc/profile; exec fish'
+    exec bash -c 'source /etc/profile; exec $(grep --max-count 1 fish /etc/shells)'
 end
