cf1e0608a5a2d738f3b45596ff6b5af43f8f9a45
Bojan Stipic <inbox@bojanstipic.com>
2026-04-28 10:31:01 +0200

    [fish] Set GOPATH

--- fish/.config/fish/conf.d/20-env.fish
+++ fish/.config/fish/conf.d/20-env.fish
@@ -3,10 +3,12 @@ status is-interactive || exit 0
 fish_add_path --path --move \
     ~/.local/bin \
     ~/.local/share/cargo/bin \
+    ~/.local/share/go/bin \
     ~/.local/share/npm/bin
 set --global --export RUSTUP_HOME ~/.local/share/rustup
 set --global --export CARGO_HOME ~/.local/share/cargo
 set --global --export GRADLE_USER_HOME ~/.local/share/gradle
+set --global --export GOPATH ~/.local/share/go
 set --global --export npm_config_prefix ~/.local/share/npm
 set --global --export npm_config_cache ~/.cache/npm
 
