bb689367025610369aefd5cdd6b5ab396af34b2f
Bojan Stipic <inbox@bojanstipic.com>
2026-04-28 10:46:18 +0200

    [fish] More XDG dirs

--- fish/.config/fish/conf.d/20-env.fish
+++ fish/.config/fish/conf.d/20-env.fish
@@ -11,6 +11,15 @@ set --global --export GRADLE_USER_HOME ~/.local/share/gradle
 set --global --export GOPATH ~/.local/share/go
 set --global --export npm_config_prefix ~/.local/share/npm
 set --global --export npm_config_cache ~/.cache/npm
+set --global --export npm_config_userconfig ~/.config/npm/npmrc
+set --global --export npm_config_init_module ~/.config/npm/npm-init.js
+set --global --export npm_config_update_notifier false
+set --global --export DOCKER_CONFIG ~/.config/docker
+set --global --export KUBECONFIG ~/.config/kube/config.yaml
+set --global --export KUBECACHEDIR ~/.cache/kube
+set --global --export K9S_CONFIG_DIR ~/.config/k9s
+set --global --export AWS_CONFIG_FILE ~/.config/aws/config
+set --global --export AWS_SHARED_CREDENTIALS_FILE ~/.config/aws/credentials
 
 set --global --export EDITOR nvim
 set --global --export VISUAL nvim
